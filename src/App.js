09c9f48268c3b150364e3575d781898edb458ad7
--replace-all <ignaciobellomo@gmail.com>
2019-11-22 15:33:41 -0500

    listing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Listing from './components/Listing';
 
 function App() {
   return (
     <div className="App">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-8">
+          <div className="row">
+            <Listing/>
+            <Listing/>
+            <Listing/>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
     </div>
   );
 }
--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -1,35 +1,79 @@
 import React, { Component } from 'react'
 
 export default class Listing extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            images: ["https://images.craigslist.org/00h0h_hA8e5gVGips_600x450.jpg", 
+                "https://images.craigslist.org/00d0d_dx0edQ3Ull4_600x450.jpg",
+                "https://images.craigslist.org/00202_b6dGFjkXyqp_600x450.jpg"      
+                ]
+        }
+    }
+
+    showImages = () => {
+        let counter = true;
+        return this.state.images.map((image) => {
+            if (!counter){
+                return (
+                    <div className="carousel-item">
+                        <img src={image} alt="Slide"/>
+                    </div>
+                )
+            } else {
+                counter = false;
+                return (
+                    <div className="carousel-item active">
+                        <img src={image} alt="Slide"/>
+                    </div>
+                )
+            }
+        })
+    }
+
+    showIndicators = () => {
+        let counter = 0;
+        return this.state.images.map((image) => {
+            if (counter > 0){
+                counter++;
+                return (
+                    <li data-target="#carouselExampleIndicators" data-slide-to={counter-1} className="active"></li>
+                )
+            } else {
+                counter++;
+                return (
+                    <li data-target="#carouselExampleIndicators" data-slide-to={counter-1}></li>
+                )
+            }
+        })
+    }
+
     render() {
         return (
-            <div className="col-6">
-                <div className="row">
-                    <div className="col-12">
-                        <div id="carouselExampleControls" className="carousel slide" data-ride="carousel">
-                            <div className="carousel-inner">
-                                <div className="carousel-item active">
-                                <img className="d-block w-100" src="..." alt="First slide"/>
-                                </div>
-                                <div className="carousel-item">
-                                <img className="d-block w-100" src="..." alt="Second slide"/>
-                                </div>
-                                <div className="carousel-item">
-                                <img className="d-block w-100" src="..." alt="Third slide"/>
-                                </div>
-                            </div>
-                            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Previous</span>
-                            </a>
-                            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span className="sr-only">Next</span>
-                            </a>
-                        </div>
+            <div className="col-6 single-listing">
+                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                    <ol className="carousel-indicators">
+                        {this.showIndicators()}
+                    </ol>
+                    <div className="carousel-inner h-25">
+                        {this.showImages()}
                     </div>
+                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+                </div>
+                <div className="listing-info">
+                    <span>Listing title</span>
+                    <p>Information about listing</p>
                 </div>
             </div>
+
         )
     }
 }
