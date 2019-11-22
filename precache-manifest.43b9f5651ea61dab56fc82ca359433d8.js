fb90e910886bdc2f05535ddd3bef06d1f0f930ab
Daphne <daphne.feresin@gmail.com>
2019-11-22 13:48:34 -0500

    Updates

---
+++ precache-manifest.43b9f5651ea61dab56fc82ca359433d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9b6ef8ac78a6143864757eb5fff608",
+    "url": "/craigslist/index.html"
+  },
+  {
+    "revision": "e47267a59abc7f83bb10",
+    "url": "/craigslist/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "a7cd50c21f3579da374f",
+    "url": "/craigslist/static/js/2.6954ea36.chunk.js"
+  },
+  {
+    "revision": "e47267a59abc7f83bb10",
+    "url": "/craigslist/static/js/main.0eb1f983.chunk.js"
+  },
+  {
+    "revision": "801a13d7172f97e5de1b",
+    "url": "/craigslist/static/js/runtime-main.5419eb64.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/craigslist/static/media/logo.25bf045c.svg"
+  }
+]);
