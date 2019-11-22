392c46d2db7ea8b04bb3925a40c22de4cfeb4dad
--replace-all <ignaciobellomo@gmail.com>
2019-11-22 14:15:02 -0500

    navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Link} from 'react-router-dom'
+
+export default function NavBar (props) {
+
+        return (
+            <div>
+            
+                <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <Link to = {'/'} className="navbar-brand">Board Games Rule</Link>
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav mr-auto navbar-links">
+                        <li className="nav-item">
+                            <Link to = {'/'} className="nav-link">
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                        </ul>
+                        <form className="form-inline my-2 my-lg-0 search-bar-form" onSubmit={props.formSubmition}>
+                            <input className="form-control mr-sm-2 search-bar" type="search" value={props.searchBarText} onChange={props.searchBar} placeholder="Search for a rulebook" aria-label="Search"/>
+                            {props.searchBarResults &&
+                                <div className="auto-complete">
+                                <ul>
+                                    {props.showSuggestions()}
+                                </ul>
+                            </div>
+                            }
+                            
+                            <button className="btn btn-outline-dark my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                    </nav>
+            </div>
+        )
+}
